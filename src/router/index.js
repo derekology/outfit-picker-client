0aed4d4d3286ff103b730b08c43f4a2df678fc76
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-06-20 17:32:56 -0700

    switched to use HTML5 mode in router to avoid hash symbol in url

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import OutfitPicker from "../views/OutfitPicker.vue"
 import Closet from "../views/Closet.vue"
 
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
