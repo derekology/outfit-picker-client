ad35d45378825ec5c94e7f2843a3ccd54a246ef0
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-06-26 00:34:53 -0700

    add social login to sign in options

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -7,13 +7,22 @@
       <input type="password" v-model="password" placeholder="Password" />
       <br /><br />
       <button @click="login">Sign In</button>
+      <br /><br />
+      <p>or Sign In with:
+      <br />
+      <button @click="googlelogin" class="social-login-button">
+        <img alt="Google logo" src="../assets/google-logo.png">
+      </button>
+      <button @click="facebooklogin" class="social-login-button">
+        <img alt="Facebook logo" src="../assets/facebook-logo.png">
+      </button>
+      </p>
     </div>
-    <br /><br />
   </div>
 </template>
 
 <script>
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default {
   name: "SignIn",
@@ -29,7 +38,7 @@ export default {
       const auth = getAuth();
       signInWithEmailAndPassword(auth, this.email, this.password).then(
         function () {
-          // alert("Well done! You are now logged in.");
+          alert("Well done! You are now logged in.");
           // this.$router.replace('home')
         },
         function (err) {
@@ -38,6 +47,53 @@ export default {
       );
       this.$router.replace("closet");
     },
+    googlelogin: function () {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+
+      signInWithPopup(auth, provider).then(
+        function () {
+          alert("Well done! You are now logged in.");
+          // this.$router.replace('closet')
+        },
+        function (err) {
+          alert("Oops. " + err.message);
+        }
+      );      
+      this.$router.replace("closet");
+    },
+    facebooklogin: function () {
+      const provider = new FacebookAuthProvider();
+      const auth = getAuth();
+
+      signInWithPopup(auth, provider).then(
+        function () {
+          alert("Well done! You are now logged in.");
+          // this.$router.replace('closet')
+        },
+        function (err) {
+          alert("Oops. " + err.message);
+        }
+      );      
+      this.$router.replace("closet");
+    },
   },
 };
 </script>
+
+<style scoped>
+  .social-login-button img {
+    width: 32px;
+    padding: 10px;
+    border-radius: 100%;
+  }
+  .social-login-button img:hover {
+    background: #eaeaea;
+    transition-duration: 0.3s;
+  }
+  .social-login-button {
+    margin: 15px;
+    background: rgb(255,255,255,0%);
+    padding: 0px;
+  }
+</style>
