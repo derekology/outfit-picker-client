30f723e07cb9e496c64cb5d27d99d6ea338c36c9
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-06-26 01:24:51 -0700

    moved all login options to general no account handler, instead of social icons being secondary to email

--- src/components/NoAccountHandler.vue
+++ src/components/NoAccountHandler.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
+    <h3>Sign in</h3>
+    <p>
+    <br />
+    <button v-on:click="emaillogin" class="login-button">
+      <img alt="Email login" src="../assets/email-login.png">
+    </button>
+    <button @click="googlelogin" class="login-button">
+      <img alt="Google logo" src="../assets/google-logo.png">
+    </button>
+    <button @click="facebooklogin" class="login-button">
+      <img alt="Facebook logo" src="../assets/facebook-logo.png">
+    </button>
+    </p>
+    <div v-if="emailloginshow">
     <span v-if="!signup">
         <SignIn />
         <br/>
-        <a class="fakeLink" v-on:click="switchReg()">No account? Sign up now.</a>
     </span>
     <span v-else>
         <SignUp />
-        <br/>
         <a class="fakeLink" v-on:click="switchReg()">Have an account? Sign in now.</a>
+    </span>
+    </div>
+    <span v-if="!signup || !emailloginshow">
+        <a class="fakeLink" v-on:click="switchReg()">No account? Sign up now.</a>
     </span>
   </div>
 </template>
 
 <script>
 import SignIn from "@/components/SignIn.vue";
-import SignUp from "@/components/SignUp.vue"
+import SignUp from "@/components/SignUp.vue";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
+
 
 export default {
   name: "NoAccountHandler",
@@ -23,14 +41,54 @@ export default {
   data() {
     return {
       signup: false,
+      emailloginshow: false,
     };
   },
 
   methods: {
-      switchReg: function () {
-          this.signup = !this.signup
-          // console.log(this.signup)
-      }
+    switchReg: function () {
+        this.signup = !this.signup
+        this.emailloginshow = true
+        // console.log(this.signup)
+    },
+    emaillogin: function () {
+        this.emailloginshow = !this.emailloginshow
+        // console.log(this.emailloginshow)
+    },
+    googlelogin: function () {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+
+      signInWithPopup(auth, provider).then(
+        function () {
+          alert("Well done! You are now logged in.");
+          // this.$router.replace('closet')
+        },
+        function (err) {
+          if ( err.code != "auth/popup-closed-by-user" && err.code != "auth/cancelled-popup-request" ) {
+            alert("Oops. " + err.code);
+          }
+        }
+      );      
+      this.$router.replace("closet");
+    },
+    facebooklogin: function () {
+      const provider = new FacebookAuthProvider();
+      const auth = getAuth();
+
+      signInWithPopup(auth, provider).then(
+        function () {
+          alert("Well done! You are now logged in.");
+          // this.$router.replace('closet')
+        },
+        function (err) {
+          if ( err.code != "auth/popup-closed-by-user" && err.code != "auth/cancelled-popup-request" ) {
+            alert("Oops. " + err.code);
+          }
+        }
+      );      
+      this.$router.replace("closet");
+    },
   },
 
   components: {
@@ -40,3 +98,19 @@ export default {
 
 };
 </script>
+
+<style scoped>
+  .login-button img {
+    width: 32px;
+    padding: 10px;
+  }
+  .login-button img:hover {
+    background: #eaeaea;
+    transition-duration: 0.3s;
+  }
+  .login-button {
+    margin: 0px 15px 10px 15px;
+    background: rgb(255,255,255,0%);
+    padding: 0px;
+  }
+</style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="signin">
-    <h3>Sign in</h3>
+    <h3>Sign in with email</h3>
     <div>
       <input type="text" v-model="email" placeholder="Email" />
       <br /><br />
@@ -8,21 +8,12 @@
       <br /><br />
       <button @click="login">Sign In</button>
       <br /><br />
-      <p>or Sign in with:
-      <br />
-      <button @click="googlelogin" class="social-login-button">
-        <img alt="Google logo" src="../assets/google-logo.png">
-      </button>
-      <button @click="facebooklogin" class="social-login-button">
-        <img alt="Facebook logo" src="../assets/facebook-logo.png">
-      </button>
-      </p>
     </div>
   </div>
 </template>
 
 <script>
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default {
   name: "SignIn",
@@ -47,57 +38,6 @@ export default {
       );
       this.$router.replace("closet");
     },
-    googlelogin: function () {
-      const provider = new GoogleAuthProvider();
-      const auth = getAuth();
-
-      signInWithPopup(auth, provider).then(
-        function () {
-          alert("Well done! You are now logged in.");
-          // this.$router.replace('closet')
-        },
-        function (err) {
-          if ( err.code != "auth/popup-closed-by-user" && err.code != "auth/cancelled-popup-request" ) {
-            alert("Oops. " + err.code);
-          }
-        }
-      );      
-      this.$router.replace("closet");
-    },
-    facebooklogin: function () {
-      const provider = new FacebookAuthProvider();
-      const auth = getAuth();
-
-      signInWithPopup(auth, provider).then(
-        function () {
-          alert("Well done! You are now logged in.");
-          // this.$router.replace('closet')
-        },
-        function (err) {
-          if ( err.code != "auth/popup-closed-by-user" && err.code != "auth/cancelled-popup-request" ) {
-            alert("Oops. " + err.code);
-          }
-        }
-      );      
-      this.$router.replace("closet");
-    },
   },
 };
 </script>
-
-<style scoped>
-  .social-login-button img {
-    width: 32px;
-    padding: 10px;
-    border-radius: 100%;
-  }
-  .social-login-button img:hover {
-    background: #eaeaea;
-    transition-duration: 0.3s;
-  }
-  .social-login-button {
-    margin: 15px;
-    background: rgb(255,255,255,0%);
-    padding: 0px;
-  }
-</style>
