9bab2d1ed1e28bf40a68c07d13c65ef25db9e5a6
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-07-03 00:58:21 -0700

    trying env variable again

--- src/constants/apollo.js
+++ src/constants/apollo.js
@@ -1,12 +1,8 @@
-import { ApolloClient, InMemoryCache } from "apollo-boost";
-import { createHttpLink } from 'apollo-link-http';
-import { createApolloProvider } from '@vue/apollo-option';
+import ApolloClient from "apollo-boost";
+import { createApolloProvider } from '@vue/apollo-option'
 
 const apolloClient = new ApolloClient({
-  link: createHttpLink({
-    uri: process.env.NEXT_PUBLIC_API_URL,
-   }),   
-  cache: new InMemoryCache(),
+  uri: process.env.NEXT_PUBLIC_API_URL,
 })
 
 const apolloProvider = createApolloProvider({
