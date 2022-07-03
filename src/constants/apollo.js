33028f504e908a4bbc5a22ae64a6015e8c294fd7
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-07-02 23:54:34 -0700

    switched apollo uri to env variable

--- src/constants/apollo.js
+++ src/constants/apollo.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { createApolloProvider } from '@vue/apollo-option'
 
 const apolloClient = new ApolloClient({
-  uri: "https://outfit-picker-api.herokuapp.com/graphql"
+  uri: process.env.API_URL
 })
 
 const apolloProvider = createApolloProvider({
