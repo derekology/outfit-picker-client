15698db30d649e9d47493fde8fde1c3c1cdd41d3
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-07-03 00:38:04 -0700

    Added InMemoryCache

--- src/constants/apollo.js
+++ src/constants/apollo.js
@@ -1,11 +1,12 @@
-import { ApolloClient } from "apollo-boost";
+import { ApolloClient, InMemoryCache } from "apollo-boost";
 import { createHttpLink } from 'apollo-link-http';
 import { createApolloProvider } from '@vue/apollo-option';
 
 const apolloClient = new ApolloClient({
   link: createHttpLink({
     uri: process.env.API_URL,
-   })
+   }),   
+  cache: new InMemoryCache(),
 })
 
 const apolloProvider = createApolloProvider({
