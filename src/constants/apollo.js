e3365aa0f771814bf137606396d268adb082023d
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-07-03 01:08:58 -0700

    trying env variable again without being in any actual variables

--- src/constants/apollo.js
+++ src/constants/apollo.js
@@ -1,12 +1,8 @@
 import ApolloClient from "apollo-boost";
 import { createApolloProvider } from '@vue/apollo-option'
 
-const apolloClient = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
+export default createApolloProvider({
+  defaultClient: new ApolloClient({
+    uri: process.env.NEXT_PUBLIC_API_URL,
+  })
 })
-
-const apolloProvider = createApolloProvider({
-  defaultClient: apolloClient,
-})
-
-export default apolloProvider
