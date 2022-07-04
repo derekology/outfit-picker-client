51c93d59b95046a183fa849e2eb7aebce46808b3
Derek Woo <79357373+derekology@users.noreply.github.com>
2022-07-03 21:42:22 -0700

    changed colour of edit icon and added cancel edit function

--- src/components/ShowClothes.vue
+++ src/components/ShowClothes.vue
@@ -57,9 +57,10 @@
                   <span v-on:click="washClothing(clothing.id, !clothing.wash)"><input type="checkbox" :checked="clothing.wash"></span>
                 </td>
                 <td class="end-col" width="20">
-                  <span class="mod-btn" v-on:click="setUpdateId(clothing.id, clothing.userid)" v-show="hoverId === clothing.id && updateId != clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FF0000" width="12" height="12" viewBox="0 0 24 24"><path d="M14.078 4.232l-12.64 12.639-1.438 7.129 7.127-1.438 12.641-12.64-5.69-5.69zm-10.369 14.893l-.85-.85 11.141-11.125.849.849-11.14 11.126zm2.008 2.008l-.85-.85 11.141-11.125.85.85-11.141 11.125zm18.283-15.444l-2.816 2.818-5.691-5.691 2.816-2.816 5.691 5.689z"/></svg> </span>
-                  <span class="mod-btn" v-on:click="updateClothing(clothing.id, clothing.type, clothing.article, clothing.colour, clothing.weight)" v-show="hoverId === clothing.id && updateId === clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FF0000" width="12" height="12" viewBox="0 0 24 24"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/></svg></span>
-                  <span class="mod-btn" v-on:click="deleteClothing(clothing.id, clothing.userid)" v-show="hoverId === clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FF0000" width="12" height="12" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" /> "></svg></span>
+                  <span class="mod-btn" v-on:click="setUpdateId(clothing, clothing.id, clothing.userid)" v-show="hoverId === clothing.id && updateId != clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FFA500" width="12" height="12" viewBox="0 0 24 24"><path d="M14.078 4.232l-12.64 12.639-1.438 7.129 7.127-1.438 12.641-12.64-5.69-5.69zm-10.369 14.893l-.85-.85 11.141-11.125.849.849-11.14 11.126zm2.008 2.008l-.85-.85 11.141-11.125.85.85-11.141 11.125zm18.283-15.444l-2.816 2.818-5.691-5.691 2.816-2.816 5.691 5.689z"/></svg> </span>
+                  <span class="mod-btn" v-on:click="updateClothing(clothing.id, clothing.type, clothing.article, clothing.colour, clothing.weight)" v-show="updateId === clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#30BA00" width="12" height="12" viewBox="0 0 24 24"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/></svg></span>
+                  <span class="mod-btn" v-on:click="cancelUpdateClothing(clothing)" v-show="updateId === clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FF0000" width="12" height="12" viewBox="0 0 24 24"><path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/></svg></span>
+                  <span class="mod-btn" v-on:click="deleteClothing(clothing.id, clothing.userid)" v-show="hoverId === clothing.id && updateId != clothing.id"><svg xmlns="http://www.w3.org/2000/svg" fill="#FF0000" width="12" height="12" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" /> "></svg></span>
                 </td>
               </tr>
             </tbody>
@@ -81,6 +82,7 @@ export default {
             showIds: false,
             uid: '',
             updateId: "",
+            oClothingObj: "",
         }
     },
 
@@ -109,13 +111,21 @@ export default {
         })
       },
 
-      setUpdateId(id, userid) {
+      setUpdateId(clothing, id, userid) {
         if (userid === 'demo') {
           alert('Cannot edit demo clothing.') }
         else {
+          this.oClothingObj = Object.assign({}, clothing);
           this.updateId = id;
           // console.log(this.updateId);
         }
+      },
+
+      cancelUpdateClothing(clothing) {
+        Object.assign(clothing, this.oClothingObj);
+        // console.log(this.oClothingObj);
+        this.updateId = "";
+        this.oClothingObj = "";
       },
 
       updateClothing(id, type, article, colour, weight) {
